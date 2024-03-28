3563c963870b9790f0dfd01c78ceddc7392c3ece
Thomas PRIOUL (MASTERIS) <thomas.prioul@masteris.com>
2024-03-28 11:18:35 +0100

    Dynamic sub-header visibility behavior using SCSS and JS

--- projects/showcase/src/pages/home.component.ts
+++ projects/showcase/src/pages/home.component.ts
@@ -1,9 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostListener,
+  NgZone,
+  OnDestroy,
+  ViewChild,
+  signal,
+} from '@angular/core';
+import { MatTabNav, MatTabsModule } from '@angular/material/tabs';
 import { Table2DemoComponent } from '../components/table2-demo/table2-demo.component';
 import { FormsDemoComponent } from '../components/forms-demo/forms-demo.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ScrollDispatcher, ScrollingModule } from '@angular/cdk/scrolling';
+import { Observable, Subject, map, pairwise, startWith, takeUntil, tap } from 'rxjs';
+import { ElementRefDirective } from 'mdl-angular';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,9 +31,47 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
     // Material
     MatTabsModule,
+    ScrollingModule,
 
+    // MDL
+    ElementRefDirective,
+
+    // App
     Table2DemoComponent,
     FormsDemoComponent,
   ],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnDestroy {
+  @ViewChild(MatTabNav, { static: true }) private readonly matTabNav!: MatTabNav;
+
+  private readonly _destroy = new Subject<void>();
+
+  protected fixedLayout = signal<boolean>(false);
+
+  constructor(el: ElementRef<HTMLElement>, scroll: ScrollDispatcher, zone: NgZone) {
+    scroll
+      .scrolled()
+      .pipe(
+        takeUntil(this._destroy),
+        map(() => window.scrollY),
+        startWith(0),
+        pairwise(),
+        tap(([prev, current]) => {
+          //@ts-ignore
+          const headerHeight = this.matTabNav._elementRef.nativeElement.offsetHeight;
+          // let headerHeight: string | number = getComputedStyle(el.nativeElement).getPropertyValue(
+          //   '--header-height'
+          // );
+          // headerHeight = parseInt(headerHeight.substring(0, headerHeight.length - 2));
+          if (this.fixedLayout() && current <= headerHeight)
+            zone.run(() => this.fixedLayout.set(false));
+          else if (current > headerHeight) zone.run(() => this.fixedLayout.set(prev > current));
+        })
+      )
+      .subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy.complete();
+  }
+}
