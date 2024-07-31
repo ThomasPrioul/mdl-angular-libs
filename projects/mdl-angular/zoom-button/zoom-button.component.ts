9eed017205a4c8e2ecb4a75867db5fa3f9db5db6
Thomas PRIOUL (MASTERIS) <thomas.prioul@masteris.com>
2024-07-31 19:10:53 +0200

    Add ngModel support to zoom button component

--- projects/mdl-angular/zoom-button/zoom-button.component.ts
+++ projects/mdl-angular/zoom-button/zoom-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -9,16 +10,39 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   styleUrls: ['zoom-button.component.scss'],
   standalone: true,
   imports: [MatTooltipModule, MatIconModule, MatButtonModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => MdlZoomButtonComponent),
+      multi: true,
+    },
+  ],
 })
-export class MdlZoomButtonComponent {
+export class MdlZoomButtonComponent implements ControlValueAccessor {
+  @Input() public disabled = false;
   @Input() public maxZoom = 2;
   @Input() public minZoom = 0.8;
-
   @Input() public zoomValue: number = 1;
   @Output() public zoomValueChange = new EventEmitter<number>();
 
   public getZoomValueString() {
     return (this.zoomValue * 100).toFixed(0).toString() + ' %';
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  public writeValue(zoomValue: number): void {
+    this.zoomValue = zoomValue;
   }
 
   protected changeZoom(amount: number) {
@@ -27,9 +51,14 @@ export class MdlZoomButtonComponent {
         Math.min(Math.max(this.minZoom, this.zoomValue + amount), this.maxZoom).toFixed(1)
       ))
     );
+    this.onChange(this.zoomValue);
   }
 
   protected resetZoom() {
-    this.zoomValueChange.emit((this.zoomValue = 1));
+    this.changeZoom(1 - this.zoomValue);
   }
+
+  private onChange: (zoomValue: number) => void = () => {};
+
+  private onTouched: () => void = () => {};
 }
