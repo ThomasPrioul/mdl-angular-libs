f43d5f5b5cb241e267229d6ee29b9ba69e5ec63c
S.Fabre (wsl) <serge.fabre@gmail.com>
2025-01-29 22:14:44 +0100

    up material + uninstall zone js

--- projects/mdl-angular/side-menu-item/side-menu-item.component.ts
+++ projects/mdl-angular/side-menu-item/side-menu-item.component.ts
@@ -5,55 +5,54 @@ import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLinkActive, RouterLink } from '@angular/router';
 
-@Component({
-    selector: 'mdl-side-menu-item',
-    templateUrl: 'side-menu-item.component.html',
-    styleUrls: ['side-menu-item.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [
-        NgClass,
-        NgStyle,
-        RouterLink,
-        RouterLinkActive,
-        MatRippleModule,
-        MatButtonModule,
-        MatIconModule,
-    ]
-})
-export class MdlSideMenuItemComponent {
-  @ViewChild(RouterLinkActive) private rla?: RouterLinkActive;
-
-  @Input()
-  public level: number = 0;
-  @Input()
-  public menu!: SideMenu;
-
-  @Input()
-  public simple: boolean = false;
-
-  @HostBinding('class.simple') private get _simple() {
-    return this.simple;
-  }
-
-  protected get expanded() {
-    return this.menu.expanded || (this.rla && this.rla.isActive);
-  }
-
-  protected toggleItem(event: MouseEvent) {
-    this.menu.expanded = !this.menu.expanded;
-  }
-}
-
 export interface SideMenu {
   children?: SideMenu[];
   clickable?: boolean;
   expanded?: boolean;
   icon?: string;
-  svgIcon?: string;
   link: string;
   ripple?: boolean;
   rippleColor?: string;
   slot?: string;
   style?: string;
+  svgIcon?: string;
   text: string;
+}
+
+@Component({
+  selector: 'mdl-side-menu-item',
+  templateUrl: 'side-menu-item.component.html',
+  styleUrls: ['side-menu-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    NgClass,
+    NgStyle,
+    RouterLink,
+    RouterLinkActive,
+    MatRippleModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+})
+export class MdlSideMenuItemComponent {
+  @ViewChild(RouterLinkActive) private readonly rla?: RouterLinkActive;
+
+  @Input()
+  public level: number = 0;
+  @Input()
+  public menu!: SideMenu;
+  @Input()
+  public simple: boolean = false;
+
+  protected get expanded() {
+    return this.menu.expanded || (this.rla && this.rla.isActive);
+  }
+
+  @HostBinding('class.simple') private get _simple() {
+    return this.simple;
+  }
+
+  protected toggleItem(event: MouseEvent) {
+    this.menu.expanded = !this.menu.expanded;
+  }
 }
