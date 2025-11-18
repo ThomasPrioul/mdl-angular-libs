6197f44d76157681207a1ae5d46d11bd07543995
Thomas PRIOUL (MASTERIS) <thomas.prioul@masteris.com>
2025-11-18 07:04:16 +0000

    Update showcase

--- docs/chunk-ISQMDEIQ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-EHTNST2Q.js";import"./chunk-K7VZ4ECY.js";import"./chunk-7DM3P2DT.js";import"./chunk-4OKUZUL7.js";export{a as FormsDemoComponent};
---
+++ docs/chunk-VIDENI5Z.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-XIJ2XMSC.js";import"./chunk-K7VZ4ECY.js";import"./chunk-WVFZ7UWP.js";import"./chunk-4OKUZUL7.js";export{a as Table2DemoComponent};
